6cc33d56531ce389c06043855998d405a5843bd6
Евгений Мишарев <misharevea@gmail.com>
2023-02-21 13:36:26 +0300

    Добавьте файлы проекта.

---
+++ CreateWallElevation/CreateWallElevationCommand.cs
@@ -0,0 +1,29 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateWallElevation
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    class CreateWallElevationCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            //Вызов формы
+            CreateWallElevationWPF createWallElevationWPF = new CreateWallElevationWPF(doc);
+            createWallElevationWPF.ShowDialog();
+            if (createWallElevationWPF.DialogResult != true)
+            {
+                return Result.Cancelled;
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ CreateWallElevation/CreateWallElevationWPF.xaml.cs
@@ -0,0 +1,90 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CreateWallElevation
+{
+    public partial class CreateWallElevationWPF : Window
+    {
+        Document Doc;
+        public List<ViewFamilyType> ViewFamilyTypeList;
+        public CreateWallElevationWPF(Document doc)
+        {
+            Doc = doc;
+            InitializeComponent();
+        }
+
+        private void btn_Ok_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+
+        private void btn_Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+        private void CreateWallElevationWPF_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter || e.Key == Key.Space)
+            {
+                DialogResult = true;
+                Close();
+            }
+
+            else if (e.Key == Key.Escape)
+            {
+                DialogResult = false;
+                Close();
+            }
+        }
+
+        private void UseToBuildCheckedChanged(object sender, RoutedEventArgs e)
+        {
+            string useToBuildSelectedName = (groupBox_UseToBuild.Content as System.Windows.Controls.Grid)
+                .Children.OfType<RadioButton>()
+                .FirstOrDefault(rb => rb.IsChecked.Value == true)
+                .Name;
+            if (useToBuildSelectedName == "rbt_Section")
+            {
+                ViewFamilyTypeList = new FilteredElementCollector(Doc)
+                    .OfClass(typeof(ViewFamilyType))
+                    .WhereElementIsElementType()
+                    .Cast<ViewFamilyType>()
+                    .Where(vft => vft.ViewFamily == ViewFamily.Section)
+                    .OrderBy(vft => vft.Name, new AlphanumComparatorFastString())
+                    .ToList();
+            }
+            if (useToBuildSelectedName == "rbt_Facade")
+            {
+                ViewFamilyTypeList = new FilteredElementCollector(Doc)
+                    .OfClass(typeof(ViewFamilyType))
+                    .WhereElementIsElementType()
+                    .Cast<ViewFamilyType>()
+                    .Where(vft => vft.ViewFamily == ViewFamily.Elevation)
+                    .OrderBy(vft => vft.Name, new AlphanumComparatorFastString())
+                    .ToList();
+            }
+
+            comboBox_SelectTypeSectionFacade.ItemsSource = ViewFamilyTypeList;
+            comboBox_SelectTypeSectionFacade.DisplayMemberPath = "Name";
+            if(ViewFamilyTypeList.Count != 0)
+            {
+                comboBox_SelectTypeSectionFacade.SelectedItem = comboBox_SelectTypeSectionFacade.Items[0];
+            }
+        }
+    }
+}
